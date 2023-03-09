234588f74c0d307f4eec0b0b5af13858742782a4
Luukas Suomi <suomiluukas@gmail.com>
2023-03-08 18:54:26 -0800

    no more if else clauses, switch statement used instead

--- main.c
+++ main.c
@@ -75,123 +75,111 @@ int main (){
     printf("Enter 'H' for a list of useable commands and '!' to close the program\n");
 
     while(buffer != '!'){
+
         buffer = getchar();
-        getchar();
+        if(buffer != '\n'){
+                switch(buffer){
+                case 'C':
+                    Create(0);
+                    break;
+                case 'F':
+                    Fork(0);
+                    break;
+                case 'K':
+                    Kill(0);
+                    break;
+                case'E':
+                    Exit(0);
+                    break;         
+                case 'Q':
+                    Quantum(0);
+                    break; 
+                case 'S':
+                    Send(0);
+                    break;           
+                case 'R':
+                    Receive(0);
+                    break;            
+                case 'Y':
+                    Reply(0);
+                    break;            
+                case 'N':
+                    New_semaphore(0);
+                    break;           
+                case 'P':
+                    P_semaphore(0);
+                    break;          
+                case 'V':
+                    V_semaphore(0);
+                    break;           
+                case 'I':
+                    Process_info(0);
+                    break;      
+                case 'T':
+                    Total_info(0);
+                    break;          
+                //this will be the help section, if the user inputs H
+                //Then the program will print some helpful info
+                case 'H':
+                    printf("This operating system supports 14 operations: (To close the system enter '!')\n");
+                    printf("\n");
 
-        if(buffer == 'C'){
-            Create(0);
-            continue;
-        }
-        else if(buffer == 'F'){
-            Fork(0);
-            continue;
-        }
-        else if(buffer == 'K'){
-            Kill(0);
-            continue;
-        }
-        else if(buffer == 'E'){
-            Exit(0);
-            continue;
-        }
-        else if(buffer == 'Q'){
-            Quantum(0);
-            continue;
-        }
-        else if(buffer == 'S'){
-            Send(0);
-            continue;
-        }
-        else if(buffer == 'R'){
-            Receive(0);
-            continue;
-        }
-        else if(buffer == 'Y'){
-            Reply(0);
-            continue;
-        }
-        else if(buffer == 'N'){
-            New_semaphore(0);
-            continue;
-        }
-        else if(buffer == 'P'){
-            P_semaphore(0);
-            continue;
-        }
-        else if(buffer == 'V'){
-            V_semaphore(0);
-            continue;
-        }
-        else if(buffer == 'I'){
-            Process_info(0);
-            continue;
-        }
-        else if(buffer == 'T'){
-            Total_info(0);
-            continue;
-        }
-        //this will be the help section, if the user inputs H
-        //Then the program will print some helpful info
-        else if(buffer == 'H'){
-            printf("This operating system supports 14 operations: (To close the system enter '!')\n");
-            printf("\n");
+                    printf("C (create): Creates a process and puts it on the appropriate ready queue\n");
+                    printf("----> Parameters for C (create): int priority, 0 = high, 1 = norm, 2 = low.\n");
+                    printf("\n");
 
-            printf("C (create): Creates a process and puts it on the appropriate ready queue\n");
-            printf("----> Parameters for C (create): int priority, 0 = high, 1 = norm, 2 = low.\n");
-            printf("\n");
+                    printf("F (fork): Copy the currently running process and put it on the ready queue corresponding to the original process' priority.\n");
+                    printf("----> Parameters for F (fork): NONE\n");
+                    printf("\n");
 
-            printf("F (fork): Copy the currently running process and put it on the ready queue corresponding to the original process' priority.\n");
-            printf("----> Parameters for F (fork): NONE\n");
-            printf("\n");
+                    printf("K (kill): Kill the named process and remove it from the system\n");
+                    printf("----> Parameters for K (kill): int pid, the process id corresponding to the process to be killed\n");
+                    printf("\n");
 
-            printf("K (kill): Kill the named process and remove it from the system\n");
-            printf("----> Parameters for K (kill): int pid, the process id corresponding to the process to be killed\n");
-            printf("\n");
+                    printf("E (exit): Kill the currently running process\n");
+                    printf("----> Parameter for E (exit): NONE\n");
+                    printf("\n");
 
-            printf("E (exit): Kill the currently running process\n");
-            printf("----> Parameter for E (exit): NONE\n");
-            printf("\n");
+                    printf("Q (quantum): time quantum of running process expires\n");
+                    printf("----> Parameters for Q (quantum): NONE\n");printf("\n");
+                    printf("\n");
 
-            printf("Q (quantum): time quantum of running process expires\n");
-            printf("----> Parameters for Q (quantum): NONE\n");printf("\n");
-            printf("\n");
+                    printf("S (send): send a message to another process -- block until it gets a reply\n");
+                    printf("----> Parameters for S (send): pid, process id of the process to send to, char * msg, the message to be sent (40 char max)\n");
+                    printf("\n");
 
-            printf("S (send): send a message to another process -- block until it gets a reply\n");
-            printf("----> Parameters for S (send): pid, process id of the process to send to, char * msg, the message to be sent (40 char max)\n");
-            printf("\n");
+                    printf("R (receive): receive a message -- block until one arrives\n");
+                    printf("----> Parameters for R (receive): NONE\n");
+                    printf("\n");
 
-            printf("R (receive): receive a message -- block until one arrives\n");
-            printf("----> Parameters for R (receive): NONE\n");
-            printf("\n");
+                    printf("Y (reply): unblocks the sending process and delivers a reply\n");
+                    printf("----> Parameters for Y (reply): int pid, process id of the process we are replying too, char * msg, message to reply (40 char max)\n");
+                    printf("\n");
 
-            printf("Y (reply): unblocks the sending process and delivers a reply\n");
-            printf("----> Parameters for Y (reply): int pid, process id of the process we are replying too, char * msg, message to reply (40 char max)\n");
-            printf("\n");
+                    printf("N (new semaphore): initialize the named semaphore with the value given. Value can be 0-4\n");
+                    printf("----> Parameters for N (new semaphore): int sid, integer value representing the semaphore can be 0-4.\n");
+                    printf("\n");
 
-            printf("N (new semaphore): initialize the named semaphore with the value given. Value can be 0-4\n");
-            printf("----> Parameters for N (new semaphore): int sid, integer value representing the semaphore can be 0-4.\n");
-            printf("\n");
+                    printf("P (semaphore lock): lock the given semaphore\n");
+                    printf("----> Parameters for P (semaphore lock): int sid, semaphore id\n");
+                    printf("\n");
 
-            printf("P (semaphore lock): lock the given semaphore\n");
-            printf("----> Parameters for P (semaphore lock): int sid, semaphore id\n");
-            printf("\n");
+                    printf(" V (semaphore unlock): unlock the corresponding semaphore\n");
+                    printf("----> Parameters for V (semaphore unlock): int sid, semaphore id \n");
+                    printf("\n");
 
-            printf(" V (semaphore unlock): unlock the corresponding semaphore\n");
-            printf("----> Parameters for V (semaphore unlock): int sid, semaphore id \n");
-            printf("\n");
+                    printf("I (process info): dump complete state information of process to screen\n");
+                    printf("----> Parameters for I (process info): int pid, process id you would like info on\n");
+                    printf("\n");
 
-            printf("I (process info): dump complete state information of process to screen\n");
-            printf("----> Parameters for I (process info): int pid, process id you would like info on\n");
-            printf("\n");
-
-            printf("T (total info): display all available information on processes and the states of the queues\n");
-            printf("----> Parameters for T (total info): NONE\n");
-            continue;
-        }else if(buffer == '!'){
-            continue;
-        }else{
-            printf("Command not recognised. Enter 'H' for help or '!' to close the program \n");
-            continue;
-        }
-    }
-}
+                    printf("T (total info): display all available information on processes and the states of the queues\n");
+                    printf("----> Parameters for T (total info): NONE\n");
+                    break;
+                case '!':
+                    break;
+                default:
+                    printf("Command not recognised. Enter 'H' for help or '!' to close the program \n");
+            }//end switch buffer
+        }// end if(buffer != '\n')
+    }//end while(buffer != '!')
+}//end int main
